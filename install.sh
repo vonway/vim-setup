3e0036afeb1e4bb3a74a081ac760e335145490c4
vonway <haisayu@gmail.com>
2016-05-27 04:18:12 -0400

    fix batch bugs

--- install.sh
+++ install.sh
@@ -1,12 +1,14 @@
 #/bin/bash
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
 if [ -f "~/.vimrc" ]; then
  mv ~/.vimrc ~/.vimrc.bak
- cp ./vimrc ~/.vimrc
 fi
+cp ./vimrc ~/.vimrc
 
 if [ ! -d "~/.vim" ];then
  mkdir -p ~/.vim/view
 fi
 
 vim +PluginInstall +qall
+
--- uninstall.sh
+++ uninstall.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 rm -rf ~/.vim
 mkdir -p ~/.vim/view
-rm ~/.vimrc
+rm ~/.vimrc ~/.viminfo
 
 if [ -f ~/.vimrc.bak ];then 
  mv ~/.vimrc.bak ~/.vimrc
